4dde556ad900d3b75ef486ca74710aab83accb00
Daniel Martí <mvdan@mvdan.cc>
2020-11-16 10:33:27 +0000

    add a build cache example to the README
    With a test-cache job on the repo's workflow, to show how it works.
    We add a third party module dependency just to show what modules are
    downloaded at each run.
    
    The GOPATH example in the README was also slightly wrong, as it did not
    set up the working directory inside GOPATH for the future steps. Fix
    that.
    
    Remove test-gopath from this repository's workflow, as having to
    download dependencies via 'go get' would make it non-reproducible and
    perhaps fail in the future. The example in the README is enough moving
    forward, as everyone should be using modules by now.
    
    Fixes #7.

--- actions.go
+++ actions.go
@@ -6,10 +6,14 @@ package actions
 import (
 	"fmt"
 	"runtime"
+
+	"rsc.io/quote"
 )
 
 func Demo() {
 	fmt.Printf("Go version: %s\n", runtime.Version())
 	fmt.Printf("GOOS: %s\n", runtime.GOOS)
 	fmt.Printf("GOARCH: %s\n", runtime.GOARCH)
+
+	fmt.Println(quote.Go())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dummy.module/actions
 
 go 1.14
+
+require rsc.io/quote v1.5.2
